c8f91b87f8d9394aa1ffc634342a9053e2c6b867
mircozenBalozen <mircea.rautoiu@gmail.com>
2023-03-23 17:40:03 +0200

    Add support for upper alpha and numeric only passwords

--- logic/commandBuilder.py
+++ logic/commandBuilder.py
@@ -15,21 +15,21 @@ def build_command(app: App):
 
     match attack_mode:
         case 0:
-            return dictionary_attack(app, base_command, hash_path)
+            return dictionary_attack(app, base_command)
         case 3:
-            return bruteforce_attack(app, base_command, hash_path)
+            return bruteforce_attack(app, base_command)
         case _:
             print("Invalid attack mode")
 
 
-def dictionary_attack(app: App, base_command, hash_path):
+def dictionary_attack(app: App, base_command):
     dictionary_file = app.options_frame.attack_mode_frame.dict_file_explorer.file_text_variable.get()
     command = base_command + f" {dictionary_file}"
     print(command)
     return command
 
 
-def bruteforce_attack(app: App, base_command, hash_path):
+def bruteforce_attack(app: App, base_command):
     option = app.options_frame.attack_mode_frame.brute_force_frame.radio_var.get()
     min_password_length = int(app.options_frame.attack_mode_frame.brute_force_frame.min_len_spinbox.spinbox.entry.get())
     max_password_length = int(app.options_frame.attack_mode_frame.brute_force_frame.max_len_spinbox.spinbox.entry.get())
@@ -38,9 +38,9 @@ def bruteforce_attack(app: App, base_command, hash_path):
         case 1:  # Lower Alpha
             command += f" {max_password_length * '?l'}"
         case 2:  # Upper Alpha
-            pass
+            command += f" {max_password_length * '?u'}"
         case 3:  # Numeric
-            pass
+            command += f" {max_password_length * '?d'}"
         case 4:  # Custom Charset
             pass
         case _:
